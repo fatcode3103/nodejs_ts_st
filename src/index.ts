46a2d22a5aefa0f01c043761956a1cda3286790a
fatcode3103 <chuvandat1112003@gmail.com>
2024-02-06 17:34:33 +0700

    fix cors

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,12 @@
 import express, { Application } from 'express'
+import cors from 'cors'
 
 import routers from './routes/index'
 
 const app: Application = express()
 const port = 8000
+
+app.use(cors())
 
 // Body parsing Middleware
 app.use(express.json())
